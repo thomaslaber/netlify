937e24e9317aab459462999d7c2174181ab15169
Thomas Laber <laber.thomas@gmail.com>
2018-12-13 12:58:40 +0100

    New stick & hide button

--- static/js/tl.js
+++ static/js/tl.js
@@ -7,11 +7,29 @@ $(document).ready(function(){
         function toggleBtnClick() {
           if (clicked) {
             $("#sticker").sticky({topSpacing:10});
-            //$("#sticker").innerHTML = "B";
+            $("#pin_button img").removeClass("deactivated");
+            $("#pin_button img").addClass("activated");
             clicked = false;
           } else {
             $("#sticker").unstick();
-            //$("#sticker").innerHTML('A');
+            $("#pin_button img").removeClass("activated");
+            $("#pin_button img").addClass("deactivated");
             clicked = true;
+          }
+        }
+		
+		var clicked2 = false;
+
+        function toggleBtnClick2() {
+          if (clicked2) {
+            $("aside").show();
+            $("#hide_button img").removeClass("deactivated");
+            $("#hide_button img").addClass("activated");
+            clicked2 = false;
+          } else {
+            $("aside").hide();
+            $("#hide_button img").removeClass("activated");
+            $("#hide_button img").addClass("deactivated");
+            clicked2 = true;
           }
         }
